157a94abfef4f76756b9e0e733e8ab8d2f4cc103
Jan van Natijne <jan.van.natijne@alten.nl>
2022-11-04 14:22:32 +0100

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,40 @@
+# alten-download-file
+
+This package was created to make downloading files easy.
+
+## Examples
+
+### Download and save
+
+It's possible to pass a fetch request directly to the download function.
+The file that is saved to the pc will be named: test.json.
+If an error occurs, the last optional argument is a callback function that receives the thrown error.
+
+If the download is successful, result will be true, otherwise false.
+
+```ts
+import {downloadFileAndSave} from "alten-download-file";
+
+const result = await downloadFileAndSave(
+    fetch("https://jsonplaceholder.typicode.com/todos/1"),
+    "test",
+    "json",
+    (e) => console.log(e)
+);
+console.log(result);
+```
+
+### Pass blob to be saved.
+
+It's also possible to pass a blob to be saved to a users pc.
+The file that is saved to the pc will be named: test.json.
+If an error occurs, the last optional argument is a callback function that receives the thrown error.
+
+```ts
+import {saveToPc} from "alten-download-file";
+
+const f = await fetch("https://jsonplaceholder.typicode.com/todo/1");
+const b = await f.blob();
+
+const result = saveToPc(b, "test", "json", (e) => console.log(e));
+```
---
+++ index.js
@@ -0,0 +1,62 @@
+/**
+ * Pass a request, filename and file type, and save the file to a local pc.
+ * The request result will be turned into a blob that will be downloaded.
+ * It is advised to use a basic Fetch for the request.
+ *
+ * Function will return true if download was a success, false if it was not.
+ *
+ * @param request
+ * @param fileName
+ * @param fileType
+ * @param onError - contains the thrown error in the callback.
+ */
+export const downloadFileAndSave = async (request, fileName, fileType, onError) => {
+    try {
+        const result = await request;
+        if (!result.ok) {
+            throw "Error in: alten-download-file -> downloadFileAndSave() -> Could not succesfully complete passed request.";
+        }
+        const blob = await result.blob();
+        if (!saveToPc(blob, fileName, fileType)) {
+            throw "Error in: alten-download-file -> downloadFileAndSave() -> Something went wrong.";
+        }
+        return true;
+    }
+    catch (e) {
+        if (onError)
+            onError(e);
+        return false;
+    }
+};
+/**
+ * Takes a blob, converts it to a file and saved it to the users pc.
+ * Returns true if download is a success, false if not.
+ * @param blob - Blob that will be downloaded.
+ * @param fileName
+ * @param fileType
+ * @param onError - contains the thrown error in the callback.
+ */
+export const saveToPc = (blob, fileName, fileType, onError) => {
+    try {
+        if (!blob) {
+            throw "Error in: alten-download-file -> saveToPc() -> Passed blob was undefined.";
+        }
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.style.display = 'none';
+        a.href = url;
+        // the filename you want
+        a.download = `${fileName}.${fileType}`;
+        document.body.appendChild(a);
+        a.click();
+        window.URL.revokeObjectURL(url);
+        // Remove link element from DOM.
+        document.body.removeChild(a);
+        return true;
+    }
+    catch (e) {
+        if (onError)
+            onError(e);
+        return false;
+    }
+};
